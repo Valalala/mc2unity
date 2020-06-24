f83417998d17740ccc9780d90310dbe63bc5ad73
Walter Rasmussen <Walter.E.O.Rasmussen@gmail.com>
2020-06-24 12:58:00 -0400

    Testing blender ops.

--- learning/testBlender.py
+++ learning/testBlender.py
@@ -1,7 +1,51 @@
 
-import subprocess
-# import bpy
+import bpy
+import time
+
+# mcWorld = input('Enter Minecraft world path: ')
+
+# terrainTex = input('Enter Mineways terrain file path: ')
+
+# print(mcWorld)
+# print(terrainTex)
 
 
-subprocess.Popen(["blender", "myScene.blend"])
+
+# mesh = bpy.data.meshes.new(name="MyMesh")
+
+# print(bpy.data.objects[1].name)
+
+# print(bpy.data.objects[1].modifier_add(type='DECIMATE'))
+
+# print(mesh.name)
+
+
+# needs to make relevant object active first.
+# bpy.ops.object.modifier_add(type='SUBSURF')
+# bpy.ops.object.modifier_apply(apply_as='DATA', modifier='Subsurf')
+# bpy.ops.object.modifier_add(type='SUBSURF')
+# bpy.context.object.modifiers['Subsurf'].levels = 2
+
+time.sleep(2)
+
+
+
+# mineways script template:
+# Minecraft world: C:\Users\erich\AppData\Roaming\.minecraft\saves\Round World
+# Terrain file name: C:\Users\erich\Desktop\Mineways\terrainExt_Sphax.png
+# Selection location: 60, 0, 60 to 80, 255, 100
+# Set render type: Wavefront OBJ absolute indices
+# Export for Rendering: C:\Users\erich\Desktop\tile00.obj
+
+# os.system('D:\Technic\mineways\mineways.exe -m D:\Technic\Repositories\mc2unity\learning\file.mwscript')
+# subprocess.run(["D:\Technic\mineways\mineways.exe", "-m", "D:\Technic\Repositories\mc2unity\learning\file.mwscript"])
+
+
+
+# remember blender script templates.
+# http://www.realtimerendering.com/erich/minecraft/public/mineways/scripting.html
+# https://docs.blender.org/api/current/bpy.ops.import_scene.html?highlight=import#module-bpy.ops.import_scene
+# https://docs.blender.org/api/current/bpy.ops.export_scene.html?highlight=export#module-bpy.ops.export_scene
+# https://medium.com/@behreajj/creative-coding-in-blender-a-primer-53e79ff71e
+
 
